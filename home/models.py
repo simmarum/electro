aad31a0c0ed421d27ef5e29f2783a30e20fba83b
simmarum <mateusz0831@gmail.com>
2020-05-25 20:46:05 +0200

    Add random of promo code

--- home/models.py
+++ home/models.py
@@ -23,6 +23,7 @@ class Product(Page):
     sku = models.CharField(max_length=255)
     short_description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    price_disc = models.TextField(blank=True, null=True)
     image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
